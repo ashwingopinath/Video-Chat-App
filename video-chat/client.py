77aebe6dd28d7352c99fa09b8ab98d5fed97a756
ysudhansh <sudhanshyelishetty@gmail.com>
2021-11-22 04:08:21 +0530

    idk

--- video-chat/client.py
+++ video-chat/client.py
@@ -2,8 +2,10 @@ import socket
 import struct
 import threading
 import cv2
+import random
 # from server import Client
 import pickle
+import numpy as np
 
 ClientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host = input("Enter server IP: ")
@@ -17,35 +19,58 @@ try:
 except socket.error as e:
     print(str(e))
 
+encode_params = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
 def inputs():
     while True:
         vid = cv2.VideoCapture(0)
         while vid.isOpened():
             ret, img = vid.read()
-            img_serialized = pickle.dumps(img)
-            msg = struct.pack("Q", len(img_serialized)) + img_serialized
-            ClientSocket.sendall(msg)
+            res, imgenc = cv2.imencode(".jpg", img, encode_params)
+            data = np.array(imgenc)
+            stringData = data.tostring()
+            # img_serialized = pickle.dumps(img)
+            # msg = struct.pack("Q", len(img_serialized)) + img_serialized
+            ClientSocket.sendall(str.encode(str(len(stringData)).ljust(16)))
+            ClientSocket.sendall(stringData)
+            # ClientSocket.sendall(msg)
+
+def recvall(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf:
+            return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
 
 def output():
-    data = b""
-    metadata_size = struct.calcsize("Q")
+    # data = b""
+    # metadata_size = struct.calcsize("Q")
+    # print("metadata_size:", metadata_size)
     while True:
-        while len(data) < metadata_size:
-            packet = ClientSocket.recv(4*1024)
-            if not packet:
-                break
-            data += packet
-        packed_msg_size = data[:metadata_size]
-        data = data[metadata_size:]
-        msg_size = struct.unpack("Q", packed_msg_size)[0]
+        # while len(data) < metadata_size:
+        #     packet = ClientSocket.recv(4*1024)
+        #     if not packet:
+        #         break
+        #     data += packet
+        # packed_msg_size = data[:metadata_size]
+        # data = data[metadata_size:]
+        # msg_size = struct.unpack("Q", packed_msg_size)[0]
 
-        while len(data) < msg_size:
-            data += ClientSocket.recv(4*1024)
-        frame_data = data[:msg_size]
-        data = data[msg_size:]
-        frame = pickle.loads(frame_data)
-        cv2.imshow("Thread", frame)
-        cv2.waitKey(10)
+        # while len(data) < msg_size:
+        #     data += ClientSocket.recv(4*1024)
+        # frame_data = data[:msg_size]
+        # data = data[msg_size:]
+        # frame = pickle.loads(frame_data)
+        length = recvall(ClientSocket, 16).decode("utf-8")
+        # print("length:", length)
+        stringData = recvall(ClientSocket, int(length))
+        data = np.fromstring(stringData, dtype="uint8")
+        imgdec = cv2.imdecode(data, cv2.IMREAD_COLOR)
+        cv2.imshow("Thread B", imgdec)
+        cv2.waitKey(1)
         
         # res = ClientSocket.recv(1024)
         # print(res.decode('utf-8'))
--- video-chat/server.py
+++ video-chat/server.py
@@ -5,6 +5,8 @@ import pickle
 import struct
 import cv2
 
+# from client import ClientSocket
+
 ServerSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host_name = socket.gethostname()
 host_ip = socket.gethostbyname(host_name)
@@ -24,19 +26,50 @@ print('Waitiing for a Connection..')
 ServerSocket.listen(5)
 print("Listening at:",socket_address) # Socket Accept
 
+def recvall(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf:
+            return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
 
 def threaded_client(connection, thread):
     connection.sendall(str.encode('\n\nWelcome to the Server\n\n'))
     while True:
         if connection:
             # vid = cv2.VideoCapture(0)
-            msg = connection.recv(4*1024)
-        else:
-            break
+            length = recvall(connection, 16)
+            # print("length:", length.decode("utf-8"))
+            stringData = recvall(connection, int(length))
+            # connection.sendall(length)
+            # connection.sendall(stringData)
+        #     data = b""
+        #     metadata_size = struct.calcsize("Q")
+        #     while len(data) < metadata_size:
+        #         packet = connection.recv(1024*1024)
+        #         if not packet:
+        #             break
+        #         data += packet
+        #     packed_msg_size = data[:metadata_size]
+        #     data = data[metadata_size:]
+        #     msg_size = struct.unpack("Q", packed_msg_size)[0]
+        #     while len(data) < msg_size:
+        #         data += connection.recv(1024*1024)
+        #     frm_data = data[:msg_size]
+        #     frm = pickle.loads(frm_data)
+        #     img_serialized = pickle.dumps(frm)
+        #     msg = struct.pack("Q", len(img_serialized)) + img_serialized
+        # else:
+        #     break
         
         for i in range(ThreadCount):
             (client, threadNo) = threads[i]
-            client.sendall(msg)
+            client.sendall(length)
+            client.sendall(stringData)
+            # client.sendall(msg)
         
             # cv2.imshow("Thread " + str(thread), img)
     
